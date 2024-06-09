34460c8e0ccbeb9b9116bb135dd6c2cec6d103b9
Felipe Maximo <felipecontato2001@hotmail.com>
2024-06-09 10:43:02 -0300

    adicionando lacamento vertical a bola e incrementacao de velocidade da mesma

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,13 +2,19 @@ extends CharacterBody2D
 
 var started = false
 var start_speed = 500
+var incremental_speed = 1.02
+var angle = [-250,250]
 
 func _physics_process(delta):
 	if Input.is_action_pressed("Start") and started == false:
 		start_game()
-		
-	move_and_collide(velocity*delta)
+	
+	if started:
+		var collision = move_and_collide(velocity*delta)
+		if collision != null:
+			velocity = velocity.bounce(collision.get_normal()) * incremental_speed
 	
 func start_game():
 	started = true
 	velocity.y = -start_speed
+	velocity.x = angle.pick_random()
