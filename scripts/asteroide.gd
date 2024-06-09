f9313623c8fe14de69782ff492b2c231596f40e3
Felipe Maximo <felipecontato2001@hotmail.com>
2024-06-09 11:32:34 -0300

    adicionando asteroids

---
+++ scripts/asteroide.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var speed = 140
+@export var rotation_speed = 1.3
+@onready var timer_delete = $TimerDelete
+
+var start_side
+
+func _ready():
+	if global_position.x > 540:
+		start_side = "right"
+	else:
+		start_side = "left"
+		
+func _process(delta):
+	if start_side == "right":
+		global_position.x -= speed * delta
+		rotation -= rotation_speed * delta
+	elif start_side == "left":
+		rotation += rotation_speed * delta
+		global_position.x += speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	timer_delete.start()
+
+
+func _on_timer_delete_timeout():
+	queue_free()
--- scripts/game.gd
+++ scripts/game.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var ball = $Ball
 @onready var label_pongs = $Pongs
 @onready var label_tutorial = $Tutorial
+@onready var positions = $Positions
+var last_postion 
+var asteroid_scene = preload("res://scenes/asteroide.tscn")
 
 func _process(delta):
 	
@@ -12,6 +15,25 @@ func _process(delta):
 	
 	label_pongs.text = str(ball.pongs)
 
-
 func _on_hole_body_entered(body):
+	call_deferred("reload_scene")
+	
+func reload_scene():
 	get_tree().reload_current_scene()
+
+
+func _on_timer_spawner_timeout():
+	spawn_asteroid()
+
+func spawn_asteroid():
+	if ball.started:
+		var positions_lilst = positions.get_children()
+		var spawn_position = positions_lilst.pick_random()
+		
+		if(spawn_position != last_postion):
+			var asteroid_instance = asteroid_scene.instantiate()
+			asteroid_instance.global_position = spawn_position.position
+			add_child(asteroid_instance)
+			last_postion = spawn_position
+		else:
+			spawn_asteroid()
